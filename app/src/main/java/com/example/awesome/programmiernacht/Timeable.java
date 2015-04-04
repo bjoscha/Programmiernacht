952ff52cf48f21e8c536b32d982e3682177a1146
Johannes <johannes@Johanness-MacBook-Pro.local>
2015-04-04 23:20:11 +0200

    Update

---
+++ app/src/main/java/com/example/awesome/programmiernacht/Timeable.java
@@ -0,0 +1,11 @@
+package com.example.awesome.programmiernacht;
+
+/**
+ * Created by johannes on 04.04.15.
+ */
+public interface Timeable {
+
+    public void tick();
+    public void timeOver();
+
+}
